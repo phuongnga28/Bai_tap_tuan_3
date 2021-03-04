3d46e70badba0f708b42383e7363bb246081651e
Phương Nga Nguyễn <78184262+phuongnga28@users.noreply.github.com>
2021-03-04 23:34:55 +0700

    Add files via upload

---
+++ Phan A - bai1a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//khong khai bao mang ngoai ham main duoc
+int main()
+{
+	int a[4];
+	for ( int i = 0; i< 4; i++)
+	{
+	cin >> a[i];}
+	for ( int i = 0; i< 4; i++)
+	{
+	cout << a[i] << " ";
+	}
+}
+
