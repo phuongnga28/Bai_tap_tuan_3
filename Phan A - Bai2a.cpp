944c9d79addf2ae48db2249cc3de18c0a7484dce
Phương Nga Nguyễn <78184262+phuongnga28@users.noreply.github.com>
2021-03-04 23:26:32 +0700

    Add files via upload

---
+++ Phan A - Bai2a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string s;
+	cin >> s;
+	cout << s << endl;;
+	for(int i=0;i<s.length();i++);
+	cout << s;
+	return 0;
+}
---
+++ Phan A - Bai2b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string s="nga";
+	cout << s<< endl;
+	for(int i=0;i<s.length();i++);
+	cout << s;
+	return 0;
+}
